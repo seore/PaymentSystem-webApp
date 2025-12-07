d706f88d5a67ad7e905bdd12a30e64a72c87b17a
Seore0102 <seoresoyannwo@gmail.com>
2025-12-07 05:06:11 +0000

    Safe updates

--- webapps/webapps2025/webapps2025/settings.py
+++ webapps/webapps2025/webapps2025/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = "change-me-in-production"
 
